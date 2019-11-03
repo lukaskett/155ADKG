c4b0c90773895bf9897a5492306006340bf8114d
lukaskett <k.lukas15@seznam.cz>
2019-11-03 22:24:00 +0100

    generating algorithms

--- U2/U2_ConvexHull/draw.cpp
+++ U2/U2_ConvexHull/draw.cpp
@@ -1,6 +1,7 @@
 #include "draw.h"
 #include <QtGui>
 #include <QMessageBox>
+#include <fstream>
 
 Draw::Draw(QWidget *parent) : QWidget(parent)
 {
@@ -128,6 +129,93 @@ void Draw::generatePoints(int method, int count_points, int width, int height)
             points.push_back(QPoint(x,y));
         }
     }
+}
+
+std::vector<QPoint> Draw::generatePointsU2(int method, int count_points, int width, int height)
+{
+    //Generate points according to selected values - type and number of points
+    //method: 1-raster, 2-random, 3-circle
+
+    //Return vector
+    std::vector<QPoint> gen_points;
+
+    //Distance from the boundary of the window - experimental value
+    int boundary_gap = 4;
+
+    //Add four corner points QPoint(x,y)
+    //left-upper
+    //points.push_back(QPoint(boundary_gap, boundary_gap));
+    //left-lower
+    //points.push_back(QPoint(boundary_gap, height - boundary_gap));
+    //right-lower
+    //points.push_back(QPoint(width - boundary_gap,height - boundary_gap));
+    //right-upper
+    //points.push_back(QPoint(width - boundary_gap, boundary_gap));
+
+
+    //Raster distribution
+    if(method == 0)
+    {
+        double step = sqrt(count_points);
+        int step_w = width / step;
+        int step_h = height / step;
+
+        for(int i = boundary_gap; i < (height - boundary_gap); i = i + step_h)
+        {
+            for(int j = boundary_gap; j < (width - boundary_gap); j = j + step_w)
+            {
+                gen_points.push_back(QPoint(j,i));
+            }
+        }
+
+        //Get exact amount of points
+        while(gen_points.size() > count_points)
+        {
+            gen_points.pop_back();
+        }
+    }
+
+    //Random distribution
+    //https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
+    else if(method == 1)
+    {
+        std::random_device rd; // obtain a random number from hardware
+        std::mt19937 eng(rd()); // seed the generator
+        std::uniform_int_distribution<> distr_x(boundary_gap, width - boundary_gap); // define the x range
+        std::uniform_int_distribution<> distr_y(boundary_gap, height - boundary_gap); // define the y range
+
+        for(int n = 0; n < count_points; n++)
+        {
+            //Generate values
+            int x = distr_x(eng);
+            int y = distr_y(eng);
+
+            //Add generated point to vector of points
+            gen_points.push_back(QPoint(x,y));
+        }
+
+    }
+
+    //Circle distribution
+    //https://codereview.stackexchange.com/questions/165338/distributing-points-uniformly-in-a-unit-circle
+    else
+    {
+        int stripeWidth = 100;
+
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::mt19937 circ(seed); // seed the generator
+        std::uniform_int_distribution<> angleDistribution(0,360);
+        std::uniform_int_distribution<> radiusDistribution(height / 2 - stripeWidth, height /2 );
+
+        for(int i = 0; i < count_points; i++) {
+            double angle  = angleDistribution(circ);
+            int radius = radiusDistribution(circ);
+            int x = width / 2 + radius * sin(angle);
+            int y = height / 2 + radius * cos(angle);
+            gen_points.push_back(QPoint(x,y));
+        }
+    }
+    return gen_points;
 }
 
 
