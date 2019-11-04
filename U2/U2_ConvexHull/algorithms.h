d1b2d2b55b7acd9810adbc4c9be81f73fb2d758c
lukaskett <k.lukas15@seznam.cz>
2019-11-04 10:47:44 +0100

    move to private

--- U2/U2_ConvexHull/algorithms.h
+++ U2/U2_ConvexHull/algorithms.h
@@ -5,8 +5,15 @@
 
 class Algorithms
 {
+private:
+    static QPoint pivot;
+    static QPoint kolinear_X_point;
+
 public:
     Algorithms();
+    //static QPoint pivot;
+    //static QPoint kolinear_X_point;
+
     static int getPointLinePosition(QPoint &q,QPoint &p1,QPoint &p2);
     static double getAngle2Vectors(QPoint &p1,QPoint &p2,QPoint &p3,QPoint &p4);
     static QPolygon jarvisScan(std::vector<QPoint> &points);
@@ -17,8 +24,7 @@ public:
     static void qh(unsigned int start, unsigned int end, std::vector<QPoint> &points,QPolygon &convexHull);
     static bool Angle(QPoint &p1, QPoint &p2);
     static bool Distance(QPoint &p1, QPoint &p2);
-    static QPoint pivot;
-    static QPoint kolinear_X_point;
+
 };
 
 #endif // ALGORITHMS_H
